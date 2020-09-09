3130f15d77edfdea2fd3da3b9edf6cf509b061e6
wwender26 <wender_hi@yahoo.com.br>
2020-09-09 13:18:59 -0300

    Persistindo os Dados

--- lists/tests.py
+++ lists/tests.py
@@ -5,6 +5,7 @@ from django.http import HttpRequest
 from lists.views import home_page
 
 from django.test import TestCase
+from lists.models import Item
 
 class HomePageTest(TestCase):
 
@@ -27,3 +28,5 @@ class HomePageTest(TestCase):
       response = self.client.post('/', data={'item_text': 'A new list item'})
       self.assertIn('A new list item', response.content.decode())
       self.assertTemplateUsed(response, 'home.html')
+	  
+
